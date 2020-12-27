0cdd6abdaa7bb6faaac317da58ccd168bd4fd5c7
Mike Holm <coldpour@gmail.com>
2020-12-27 11:29:38 -0800

    add venmo logo

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,9 +2,11 @@
 import LinksSection from "./LinksSection";
 import FallPhoto from "./FallPhoto";
 import PolaroidSection from "./PolaroidSection";
+import LogoSection from "./LogoSection";
 
 const HomePage = () => (
   <div>
+    <LogoSection />
     <PolaroidSection />
     <LinksSection />
     <FallPhoto />
