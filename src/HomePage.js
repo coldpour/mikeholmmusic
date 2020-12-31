66d1d6ea735bd55205d56651d4872ba0da83dff6
Mike Holm <coldpour@gmail.com>
2020-12-30 16:38:35 -0800

    clean up home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,17 +1,12 @@
 /** @jsxImportSource @emotion/react */
-import LinksSection from "./LinksSection";
-import FallPhoto from "./FallPhoto";
 import PolaroidSection from "./PolaroidSection";
 import LogoSection from "./LogoSection";
 import MenuSection from "./MenuSection";
 
 const HomePage = () => (
   <div>
-    <LogoSection />
-    <MenuSection />
     <PolaroidSection />
-    <LinksSection />
-    <FallPhoto />
+    <MenuSection id="menu" />
     <LogoSection />
   </div>
 );
