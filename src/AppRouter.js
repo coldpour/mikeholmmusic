5b189f8e584f58eafba90ddb5d1d1fc85e8d8a72
Mike Holm <coldpour@gmail.com>
2020-12-31 00:03:01 -0800

    remove links route

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,15 +1,11 @@
 /** @jsxImportSource @emotion/react */
 import HomePage from "./HomePage";
-import LinksPage from "./LinksPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const AppRouter = (props) => {
   return (
     <Router {...props}>
       <Switch>
-        <Route path="/links">
-          <LinksPage />
-        </Route>
         <Route path="/">
           <HomePage />
         </Route>
