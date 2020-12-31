f2616ce5940bad2a6fa16deff57084597dea3bbf
Mike Holm <coldpour@gmail.com>
2020-12-30 16:35:14 -0800

    touch up polaroid styles

--- src/Polaroid.js
+++ src/Polaroid.js
@@ -1,11 +1,18 @@
 /** @jsxImportSource @emotion/react */
 import styled from "@emotion/styled";
 
-const Polaroid = styled.div`
+const Root = styled.div`
   background: white;
-  padding: 0.5em 0.5em 3em;
+  padding: 8px;
   ${(props) => props.theme.square("300px")}
-  box-shadow: ${(props) => props.theme.shadow[3]}
+  box-shadow: ${(props) => props.theme.shadow[3]};
+  padding-bottom: 20px;
+  @media (min-width: 600px) {
+    padding-bottom: 60px;
+  }
 `;
 
+const Polaroid = ({ children, title, ...rest }) => (
+  <Root {...rest}>{children}</Root>
+);
 export default Polaroid;
