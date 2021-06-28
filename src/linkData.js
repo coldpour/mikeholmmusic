6416ce2e4a411c3b15f77387ecb4b15b16ae1feb
Mike Holm <coldpour@gmail.com>
2021-06-27 23:56:09 -0700

    organize link data

--- src/linkData.js
+++ src/linkData.js
@@ -1,55 +1,60 @@
-import ig from "./ig.png";
-import yt from "./yt.png";
-import tiktok from "./tiktok.png";
-import soundcloud from "./soundcloud.png";
-import venmoLogo from "./venmo.png";
 import * as Logos from "./Logos";
 
 export const Venmo = {
   logo: Logos.VenmoLogo,
-  src: venmoLogo,
   alt: "venmo logo",
   href: "https://venmo.com/themikeholm",
   label: "Venmo",
 };
 
-const linkData = [
-  {
-    logo: Logos.TikTokLogo,
-    src: tiktok,
-    alt: "tiktok logo",
-    href: "https://www.tiktok.com/@themikeholm",
-    label: "TikTok",
-  },
-  {
-    logo: Logos.InstagramLogo,
-    src: ig,
-    alt: "instagram logo",
-    href: "https://www.instagram.com/themikeholm/",
-    label: "Instagram",
-  },
-  {
-    logo: Logos.YouTubeLogo,
-    src: yt,
-    alt: "youtube logo",
-    href: "https://www.youtube.com/channel/UCSRAICuM9exsw09Qw1WTKKw",
-    label: "YouTube",
-  },
-  {
-    logo: Logos.SoundCloudLogo,
-    src: soundcloud,
-    alt: "soundcloud logo",
-    href: "https://soundcloud.com/mike-holm-173325201",
-    label: "SoundCloud",
-  },
-  Venmo,
-  {
-    logo: Logos.CashAppLogo,
-    src: "",
-    alt: "cash app logo",
-    href: "https://cash.app/$themikeholm",
-    label: "Cash App",
-  },
-];
+export const TikTok = {
+  logo: Logos.TikTokLogo,
+  alt: "tiktok logo",
+  href: "https://www.tiktok.com/@themikeholm",
+  label: "TikTok",
+};
+
+export const Instagram = {
+  logo: Logos.InstagramLogo,
+  alt: "instagram logo",
+  href: "https://www.instagram.com/themikeholm/",
+  label: "Instagram",
+};
+
+export const YouTube = {
+  logo: Logos.YouTubeLogo,
+  alt: "youtube logo",
+  href: "https://www.youtube.com/channel/UCSRAICuM9exsw09Qw1WTKKw",
+  label: "YouTube",
+};
+
+export const SoundCloud = {
+  logo: Logos.SoundCloudLogo,
+  alt: "soundcloud logo",
+  href: "https://soundcloud.com/mike-holm-173325201",
+  label: "SoundCloud",
+};
+
+export const CashApp = {
+  logo: Logos.CashAppLogo,
+  alt: "cash app logo",
+  href: "https://cash.app/$themikeholm",
+  label: "Cash App",
+};
+
+export const Facebook = {
+  logo: Logos.FacebookLogo,
+  alt: "facebook logo",
+  label: "Facebook",
+  href: "https://www.facebook.com/mholm815",
+};
+
+export const DM = [TikTok, Instagram, Facebook];
+
+export const Tip = [Venmo, CashApp];
+
+export const Listen = [YouTube, SoundCloud];
+
+const linkData = [...DM, ...Listen, ...Tip];
 
 export default linkData;
