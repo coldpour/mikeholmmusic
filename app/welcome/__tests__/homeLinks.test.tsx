8f5ed73f60df5ec2f49dbd8d47280acf4eea52eb
coldpour <coldpour@gmail.com>
2026-02-08 22:05:46 -0800

    feat: link home releases to internal pages

---
+++ app/welcome/__tests__/homeLinks.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import { describe, expect, it } from "vitest";
+import { Welcome } from "../welcome";
+
+describe("home release links", () => {
+  it("links release cards to internal pages", () => {
+    render(
+      <MemoryRouter>
+        <Welcome />
+      </MemoryRouter>
+    );
+
+    const link = screen.getByRole("link", { name: /Squat!/i });
+    expect(link).toHaveAttribute("href", "/releases/squat");
+  });
+});
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router";
+import { releases } from "../data/releases";
+
 export function Welcome() {
-  const [head, ...rest] = promo;
+  const [head, ...rest] = releases;
   return (
     <main className="flex items-center justify-center pt-8 md:pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-4 md:gap-8 min-h-0 max-w-xl px-4">
@@ -20,7 +23,11 @@ export function Welcome() {
           </ul>
           <Album {...head} />
         </header>
-        <section className="grid grid-cols-2 gap-4">{rest.map(Album)}</section>
+        <section className="grid grid-cols-2 gap-4">
+          {rest.map((release) => (
+            <Album key={release.slug} {...release} />
+          ))}
+        </section>
         <div className="max-w-[300px] w-full px-12">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
             <ul>
@@ -51,17 +58,16 @@ export function Welcome() {
   );
 }
 
-function Album({ href, label, image }: Promo) {
+function Album({ slug, title, coverImage }: (typeof releases)[number]) {
   return (
-    <a
-      key={href}
+    <Link
+      key={slug}
       className="flex flex-col items-start gap-4 self-stretch leading-normal transition pb-4 hover:scale-105"
-      href={href}
-      rel="noreferrer"
+      to={`/releases/${slug}`}
     >
-      <img src={image} alt={label} className="rounded-md" />
-      <h4 className="text-xs md:text-lg">{label}</h4>
-    </a>
+      <img src={coverImage} alt={title} className="rounded-md" />
+      <h4 className="text-xs md:text-lg">{title}</h4>
+    </Link>
   );
 }
 
@@ -160,90 +166,6 @@ export const Facebook = {
   href: "https://www.facebook.com/themikeholm",
 };
 
-export const TrashPandas = {
-  label: "The Trash Pandas",
-  href: "https://thetrashpandas.com",
-};
-
-interface Promo {
-  label: string;
-  href: string;
-  image: string;
-}
-
-export const Sandcastles = {
-  label: "Straight Lines Bright Colors",
-  href: "https://linktr.ee/sandcastles",
-  image:
-    "http://lh3.googleusercontent.com/kbGM2wOVRuBFzSj8L2nlze8ADUOFD5DjoEu1jZwLzMfoBcCLe6vdZK_CnYHLcPJ_SahN285HuFgARiPWMA=w544-h544-l90-rj",
-};
-
-export const Bandcamp = {
-  label: "Bandcamp",
-  href: "https://mikeholm.bandcamp.com/",
-};
-
-export const NakedCharades = {
-  label: "Naked Charades",
-  href: "https://distrokid.com/hyperfollow/mikeholm/naked-charades",
-  image:
-    "http://lh3.googleusercontent.com/7OxNCAgC47Cf9J5gHCuhZcRdqIDkXpyBqa12ahE4ZWikHJS-AewHIXAhSBzS6ttNXSgstJeN_IO-rWi-iQ=w544-h544-l90-rj",
-};
-
-export const HeartOfFireBrainOfIce = {
-  label: "Heart of Fire, Brain of Ice",
-  href: "https://distrokid.com/hyperfollow/mikeholm/hearth-of-fire-brain-of-ice",
-  image:
-    "http://lh3.googleusercontent.com/9uxLQYbOc65uF-EIa8cpg_hUI5Gh3tbSi_H27qhWCqZwI9qLmF4M0ESqHfVcO-1t7AMOGuB-nWzh4kM=w544-h544-l90-rj",
-};
-
-export const Possibility = {
-  label: "Possibility",
-  href: "https://distrokid.com/hyperfollow/mikeholm/possibility",
-  image:
-    "http://lh3.googleusercontent.com/GL48m80uorQ9QlTX2k4wQp3t_Gda8HNV54FUR_gLcRtRVbpnM1LUa3-0nOBsd1ezvEwhXEK6gynbVn0D=w544-h544-l90-rj",
-};
-
-export const JustComeOn = {
-  label: "Just Come On",
-  href: "https://distrokid.com/hyperfollow/mikeholm/just-come-on",
-  image:
-    "http://lh3.googleusercontent.com/IMtAIAQ1GeH1pyVC9xe4p1N4ymB9G2Bba2vs92uD1jMvUqZFRJpz0Nafjk8xii_iJG22_sAnWVx2_r24Eg=w544-h544-l90-rj",
-};
-
-export const Impediments = {
-  label: "Impediments",
-  href: "https://distrokid.com/hyperfollow/mikeholm/impediments",
-  image:
-    "http://lh3.googleusercontent.com/3SMPbNC1HyvpR12i4Tf73zTFylM0PRMOAOOfuHeEVAgbAIzL3nHe9zojqY0tRUzNvrD_PnzAxAlfF3VsCA=w544-h544-l90-rj",
-};
-
-export const RiseUp = {
-  label: "Rise Up ft. Jamaal Coleman",
-  href: "https://distrokid.com/hyperfollow/mikeholm/rise-up-feat-jamaal-coleman",
-  image:
-    "http://lh3.googleusercontent.com/u0iJ_yQo0MOuBqJVbHkCMAooS0PVtYeC9rygOZzYi5bX6rNBiBXGSQ6o_OZyuKdcuFFeABsb6dJu7EnlGg=w544-h544-l90-rj",
-};
-
-export const AllWeGotIsTime = {
-  label: "All We Got Is Time",
-  href: "https://distrokid.com/hyperfollow/mikeholm/all-we-got-is-time-2",
-  image:
-    "http://lh3.googleusercontent.com/ekDMV3T0Scid196x0pb68h6OnlSjwA1uuNWHwy5F1vA_a4mcL9KAzVFZji6XcV-FgpO0wT2k7VshOvKo=w544-h544-l90-rj",
-};
-
-export const Squat = {
-  label: "Squat!",
-  href: "https://distrokid.com/hyperfollow/mikeholm/squat",
-  image:
-    "http://lh3.googleusercontent.com/yYi2eIbsoV4VR57b-B73itT46AC1slWe9WII-t2psJ2pvvUULzDxtyUEdbPa7_MhNS_v2d4VpknaHhFp=w544-h544-l90-rj",
-};
-
-export const RedKarma = {
-  label: "Red Karma, a Taylor Swift Tribute",
-  href: "https://www.redkarma13.com/",
-};
-
 export const DM = [TikTok, Instagram, Facebook];
 
 export const Tip = [Venmo, CashApp];
@@ -251,15 +173,3 @@ export const Tip = [Venmo, CashApp];
 export const Listen = [YouTube, SoundCloud];
 
 const socials = [...DM, ...Listen, ...Tip];
-
-const promo = [
-  Squat,
-  AllWeGotIsTime,
-  RiseUp,
-  Impediments,
-  JustComeOn,
-  Possibility,
-  HeartOfFireBrainOfIce,
-  NakedCharades,
-  Sandcastles,
-];
